90c584b67347b521e63cf2c2fc200bf8434e0a6d
jamielill <jl2592c@gre.ac.uk>
2025-06-22 16:40:58 +0100

    changed sizing of slider image icons

--- comparisonSlider.js
+++ comparisonSlider.js
@@ -1,4 +1,4 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.comparison-box');
 document.querySelector('.slider').addEventListener('input', (e) => {
   container.style.setProperty('--position', `${e.target.value}%`);
 })
